a0f10207481f5737c8bf6350d16d3bff87451a5c
Solarusastoras <micformweb@gmail.com>
2024-10-16 23:07:15 +0200

    suppression telephone

--- src/Composants/Footer/index.jsx
+++ src/Composants/Footer/index.jsx
@@ -9,7 +9,6 @@ function Footer() {
     <footer>
       <div>© 2024 Michael Dulou</div>
       <div>Téléphone : 06 48 71 37 27</div>
-      <div>mail : micformweb@gmail.com</div>
       <div>
         <a
           href="https://www.linkedin.com/in/michaeldulou/"
