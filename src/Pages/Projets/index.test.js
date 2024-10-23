7aa8badaf22c40b45f753e766a4cba6c69a65b94
Solarusastoras <micformweb@gmail.com>
2024-10-24 01:53:45 +0200

    correction test

--- src/Pages/Projets/index.test.js
+++ src/Pages/Projets/index.test.js
@@ -1,6 +1,6 @@
 import "whatwg-fetch";
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import {
   beforeAll,
   beforeEach,
@@ -65,70 +65,54 @@ const mockProjet = {
   gitpage: "https://solarusastoras.github.io/Projet-2/",
 };
 
-beforeEach(() => {
+beforeEach(async () => {
   localStorage.setItem("clickedId", "2");
   jest.spyOn(window, "fetch").mockImplementation(() =>
     Promise.resolve({
       json: () => Promise.resolve(mockProjet),
     })
   );
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <Projets />
+      </MemoryRouter>
+    );
+  });
 });
 
 // Test d'intégration
 describe("When a page is created", () => {
   // Test pour vérifier que le bon titre est affiché
   it("a title of site is displayed", async () => {
-    render(
-      <MemoryRouter>
-        <Projets />
-      </MemoryRouter>
-    );
     const title = await screen.findByRole("heading", { level: 1 });
     expect(title).toHaveTextContent("Riding Cities");
   });
 
   // Test pour vérifier que la description du site est affichée
   it("a description is displayed", async () => {
-    render(
-      <MemoryRouter>
-        <Projets />
-      </MemoryRouter>
-    );
-    const description = await screen.findByText(
-      "Description"
-    );
+    const description = await screen.findByText((content, element) => {
+      return content.includes(
+        "Site web d une association de Rider. Ce projet est une introduction aux fondamentaux du langage HTML et CSS. Ma tâche principale était de créer une nouvelle section sur la page web de l'association."
+      );
+    });
     expect(description).toBeInTheDocument();
   });
 
   // Test pour vérifier que les langages utilisés sont affichés
   it("languages used are displayed", async () => {
-    render(
-      <MemoryRouter>
-        <Projets />
-      </MemoryRouter>
-    );
-    const languages = await screen.findByText("HTML");
+    const languages = await screen.findByText("Langages utilisés");
     expect(languages).toBeInTheDocument();
   });
 
   // Test pour vérifier que le bouton GitHub est affiché
   it("a GitHub button is displayed", async () => {
-    render(
-      <MemoryRouter>
-        <Projets />
-      </MemoryRouter>
-    );
     const githubButton = await screen.findByText("GitHub");
     expect(githubButton).toBeInTheDocument();
   });
 
   // Test pour vérifier que le bouton GitPage est affiché (si présent)
   it("a GitPage button is displayed if present", async () => {
-    render(
-      <MemoryRouter>
-        <Projets />
-      </MemoryRouter>
-    );
     const gitpageButton = await screen.queryByText("GitPage");
     if (gitpageButton) {
       expect(gitpageButton).toBeInTheDocument();
